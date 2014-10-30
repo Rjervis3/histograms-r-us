9262e6d9ea9f70cc27c52192b6758d321663c29c
Renn Jervis <jervisre@grinnell.edu>
2014-10-29 21:49:12 -0500

    inital commit

---
+++ supProb3.c
@@ -0,0 +1,54 @@
+    /***********************************************************************
+     * Name: Renn Jervis                                                   *
+     * Box: 3762                                                           *
+     * Assignment name: Supplemental Problem Number 3                      *
+     * Assignment for Monday, November 3rd                                 *
+     ***********************************************************************/
+
+    /* *********************************************************************
+     * Academic honesty certification:                                     *
+     *   Written/online sources used:                                      *
+     *     [                              *
+     *                                    *
+     *                                    *
+     *   Help obtained: [none]                                             *
+     *                                                                     *
+     *   My/our signature(s) below confirms that the above list of sources *
+     *   is complete AND that I/we have not talked to anyone else          *
+     *   (e.g., CSC 161 students) about the solution to this problem       *
+     *                                                                     *
+     *   Signature:                                                        *
+     ***********************************************************************/
+
+
+
+
+/* This is a program to tabulate the result of a simulation. The simulation *
+*   is to determine the family size of a family that will keep having       * 
+*   chidren until they have at least one girl and one boy.                  *
+*   This program will take the results of the simulation and print a table  *
+*   as well as a histogram of the results. The height of the histogram ought*
+*   to be scaled. */ 
+
+
+
+#include <stdio.h>
+
+
+
+
+int main()
+{
+ /* the number of couples in the simulation */
+  const int numberOfCouples = 60;
+
+  /* number of children is recorded for sizes 1 .. maxRecordedSize */
+  const int maxRecordedSize = 30;
+  int famSize [maxRecordedSize + 1];
+
+  /* the highest point of the histogram will have heightOfHistogram *'s */
+  const int heightOfHistogram = 20;
+
+
+  return 0;
+}
